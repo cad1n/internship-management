daa32665146d31371a705e9a8ab2858b97021cbc
cad1n <caioaugusto4520@gmail.com>
2025-05-22 15:09:08 -0300

    Adding unit tests in the pdf rendering

--- main/settings.py
+++ main/settings.py
@@ -6,12 +6,6 @@ from django.contrib.messages import constants as messages
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
--- management_app/tests.py
+++ management_app/tests.py
@@ -1,1 +1,41 @@
-# Create your tests here.
+import pytest, os
+from unittest.mock import patch, MagicMock
+from django.http import HttpResponse
+from management_app.render import Render
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
+
+@pytest.mark.django_db
+@patch("management_app.render.get_template")
+@patch("management_app.render.pisa.pisaDocument")
+def test_render_pdf_success(mock_pisa, mock_get_template):
+    # Mock do template
+    mock_template = MagicMock()
+    mock_template.render.return_value = "<html><body>PDF</body></html>"
+    mock_get_template.return_value = mock_template
+
+    # Mock do pisaDocument
+    mock_pdf = MagicMock()
+    mock_pdf.err = False
+    mock_pisa.return_value = mock_pdf
+
+    response = Render.render("fake_template.html", {"foo": "bar"})
+    assert isinstance(response, HttpResponse)
+    assert response.status_code == 200
+    assert response["Content-Type"] == "application/pdf"
+
+@patch("management_app.render.get_template")
+@patch("management_app.render.pisa.pisaDocument")
+def test_render_pdf_error(mock_pisa, mock_get_template):
+    mock_template = MagicMock()
+    mock_template.render.return_value = "<html><body>PDF</body></html>"
+    mock_get_template.return_value = mock_template
+
+    mock_pdf = MagicMock()
+    mock_pdf.err = True
+    mock_pisa.return_value = mock_pdf
+
+    response = Render.render("fake_template.html", {"foo": "bar"})
+    assert isinstance(response, HttpResponse)
+    assert response.status_code == 400
+    assert b"Error Rendering PDF" in response.content
